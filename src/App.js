095fec7903f4f88a16a04fca2326800275707c8d
남서현 <syn5421a@naver.com>
2024-05-17 03:06:13 +0900

    UI 버튼 부분까지 포함하여 재구구현

--- src/App.js
+++ src/App.js
@@ -2,48 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Shoppingitem from './Shoppingitem';
+import ShoppingitemRow from './Shoppingitem';
 
 function App() {
   const [items, setItems] = useState([
     {
       id:"0",
       title:'Hello',
-      bookId:"sss"
+
     },{
       id:"1",
       title:'Hello',
-      bookId:"sss"
+
     }
   ]);
-
-  const headers = [
-    {
-      text: "Name",
-      value: "title"
-    },
-    {
-      text: "Book ID",
-      value: "bookId"
-    }
-  ];
 
   let shoppingItems = items.length > 0 && (
     <table style={{ margin:16, border: '1px solid black'}}>
       <thead>
         <tr>
-          {headers.map((header, index) => 
-            <th key={index} style={{ padding: '10px', border: '1px solid black'}}>{header.text}</th>
-          )}
+          <th style={{ padding: '10px', border: '1px solid black' }}>id</th>
+          <th style={{ padding: '10px', border: '1px solid black' }}>title</th>
+          <th style={{ padding: '10px', border: '1px solid black' }}>삭제 버튼</th>
         </tr>
       </thead>
       <tbody>
-        {items.map((item) => (
-          <tr key={item.id}>
-            {headers.map((header, index) => (
-              <td key={index} style={{ padding: '10px', border: '1px solid black' }}>{item[header.value]}</td>
-            ))}
-          </tr>
-        ))}
+        {items.map((item)=>(<ShoppingitemRow key={item.id} item={item} />))}
       </tbody>
     </table>
   );
--- src/Shoppingitem.js
+++ src/Shoppingitem.js
@@ -1,42 +1,18 @@
 import React, { useState } from "react";
-import {ListItem, ListItemText, InputBase} from '@mui/material';
+import {ListItem, ListItemText, InputBase, Button, ListItemButton} from '@mui/material';
 
-const Shoppingitem = (props) => {
-    const [item, setItem] = useState(props.item);
-    return(
-        <ListItem>
-            <ListItemText>
-                <InputBase InputProps={{ "aria-label":"naked" }}
-                type = "text"
-                id={item.id}
-                name={item.id}
-                value={item.title}
-                multiline={true}
-                fullWidth={true}                
-                />
-            </ListItemText>
-            <ListItemText>
-                <InputBase InputProps={{ "aria-label":"naked" }}
-                type = "text"
-                id={item.id}
-                name={item.id}
-                value={item.bookId}
-                multiline={true}
-                fullWidth={true}                
-                />
-            </ListItemText>
-            <ListItemText>
-                <InputBase InputProps={{ "aria-label":"naked" }}
-                type = "text"
-                id={item.id}
-                name={item.id}
-                value={item.bookId}
-                multiline={true}
-                fullWidth={true}                
-                />
-            </ListItemText>
-        </ListItem>
+const ShoppingitemRow = ({ item, onDelete }) => {
+    return (
+      <tr >
+        <td style={{ padding: '10px', border: '1px solid black' }}>{item.id}</td>
+        <td style={{ padding: '10px', border: '1px solid black' }}>{item.title}</td>
+        <td style={{ padding: '10px', border: '1px solid black' }}>
+          {/* <button onClick={() => onDelete(todo.id)}>delete</button> */}
+          <button>삭제</button>
+        </td>
+      </tr>
     );
-};
+  };
+  
 
-export default Shoppingitem;
+export default ShoppingitemRow;
